785a275a2416a8dde0a7600e88f2da99bfd4914b
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2024-05-20 15:34:05 -0300

    Delete Lista-4 directory

--- Lista-4/palídromo.py
+++
@@ -1,8 +0,0 @@
-#PALÍNDROMO
-
-while True:
-    palavra = input("Digite a palavra:")
-    if str(palavra) == str(palavra)[::-1] :
-        print("Palíndromo")
-    else:
-        print("Não é Palíndromo")

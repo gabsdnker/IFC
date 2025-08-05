d6c957b7aabf2b0cd16b807c80ba16b3f811803c
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-08-04 21:32:34 -0300

    Add files via upload

---
+++ SD/Sistema de arquivos/ClientDir/client.py
@@ -0,0 +1,315 @@
+
+import requests
+import json
+import os
+import time
+
+SERVER_ADDR = ["http://191.52.7.100:5000", "http://191.52.7.101:5000", "http://191.52.7.99:5000", "http://191.52.6.51:5000"]
+FILE_DIR = "./ClientDir/files"
+
+class File:
+    # construtor com valor padrão nos parâmetros
+    def __init__(self, name="", id="", modified=""):
+        self.name = name
+        self.id = id
+        self.modified = modified
+
+    # expressar a classe em formato texto
+    def __str__(self):
+        return f'{self.name}, '+\
+               f'{self.id}, {self.modified}'
+
+    # expressar a classe em formato json
+    def json(self):
+        return {
+            "name" : self.name,
+            "id" : self.id,
+            "modified" : self.modified 
+        }
+
+class Client:
+    # construtor da classe Client
+    # recebe o endereço do servidor e o diretório de arquivos
+    # e inicializa os atributos necessários
+    def __init__(self, server_addr=SERVER_ADDR, file_dir=FILE_DIR):
+        # inicializa os atributos da classe
+        self.server_addr = server_addr
+        # diretório onde os arquivos serão armazenados
+        self.file_dir = file_dir
+        # listas para armazenar os arquivos do cliente e do servidor
+        self.client_prev_list = None
+        # listas para armazenar os arquivos atuais do cliente e do servidor
+        self.client_cur_list = None
+        # listas para armazenar os arquivos do servidor
+        self.server_prev_list = None
+        # lista de operações a serem realizadas
+        self.server_cur_list = None
+        # lista de operações a serem realizadas
+        self.operation_list = []
+
+    def list_server_files(self, verbose=False):
+        # lista de arquivos do servidor
+        for addr in self.server_addr:
+            # faz uma requisição GET para o servidor
+            # e obtém a lista de arquivos
+            response = requests.get(f'{addr}/listar').json()
+            if verbose:
+                # imprime os nomes dos arquivos retornados
+                for n in response['files']:
+                    print(n['name'])
+            print(response)
+            if response:
+                # retorna a lista de arquivos do servidor
+                return response['files']
+
+    def list_local_files(self, verbose=False):
+        # lista de arquivos locais
+        files = []
+        json_files = []
+
+        dirEntrys = os.scandir(self.file_dir)
+
+        for entry in dirEntrys:
+            if entry.is_file() and entry.name != "client.py":
+                file = File(entry.name, entry.stat().st_ino, entry.stat().st_mtime)
+                print(file)
+                files.append(file)
+
+        for file in files:
+            json_files.append(file.json())
+
+        if verbose:
+            print(json_files)
+
+        return json_files
+
+    def create_file(self, file_name):
+        erro = False
+        for addr in self.server_addr:
+            response = requests.get(f'{addr}/criar/{file_name}').json()
+            if response['header'] == "OK":
+                f = open(f"{self.file_dir}/{file_name}", "w")
+                f.close()
+            else:
+                erro = True
+
+        if not erro:
+            return True
+        
+        return False
+
+
+    def delete_file(self, file_name):
+        response = requests.get(f'{self.server_addr}/deletar/{file_name}').json()
+        
+        if response['header'] == "OK":
+            os.remove(f"{self.file_dir}/{file_name}")
+            return True
+    
+        return False
+
+    def write_to_file(self, file_name, content):
+        for addr in self.server_addr:
+            response = requests.get(f'{addr}/escrever/{file_name}/{content}').json()
+            
+            if response['header'] == "OK":
+                f = open(f"{self.file_dir}/{file_name}", "w")
+                f.write(content)
+                f.close()
+        
+        return True
+    
+    def upload(self, file_name):
+        for addr in self.server_addr:
+
+            files = {'files': open(f'{self.file_dir}/{file_name}', 'rb')}
+            response = requests.post(f'{addr}/escrever', files=files).json()
+
+        return response['header'] == "OK"
+
+
+    def read_from_file(self, file_name):
+        response = requests.get(f'{self.server_addr}/ler/{file_name}').json()
+        
+        if response['header'] == "OK":
+            print(type(response['detail']))
+            return response['detail']
+    
+        return None
+
+    def update_local_files(self):
+        pass
+
+    def install(self):
+
+        # get the servers folder files list
+
+        # create locally all files recovered from the server
+
+        pass
+
+    def merge(self):
+
+        # get the servers folder files list
+
+        # load the list of local files, in the FILE_DIR folder      
+
+        # Compares and find the difference from the two dicts  
+        # example of comparison results:
+        # [('local_remove','praia.png'), 
+        # ('remote_create','texto.txt'), 
+        # ('remote_write','texto.txt','este-e-o-conteudo-d)]
+
+        pass
+
+    # Tmp methods
+    def dump(self):
+        server_files = self.list_server_files()
+        
+        for file in server_files['files']:
+            f = open(f"{self.file_dir}/{file['name']}", "w")
+            f.close()
+
+
+def test():
+    # Test the Client class
+    client_1 = Client()
+    file_name = "newFileFromClient.txt"
+    content_to_write = "conteudo-para-teste-de-escrita"
+
+    print("Initial files:")
+    # List files on the server
+    client_1.list_server_files(verbose=True)
+    #client_1.dump()
+
+    # Create the file
+    print(f"*** Creating file {file_name}")
+    client_1.create_file(file_name)
+
+    # Check if the file was created
+    print("*** The file was created?")
+    client_1.list_server_files(verbose=True)
+    
+    # Write content to the file
+    print(f"*** Writing {content_to_write} to {file_name}")
+    if client_1.write_to_file(file_name, content_to_write):
+        print("Success in writing")
+    else:
+        print("I could not write :-(")
+    
+    print("The content was written? Testing file reading...")
+    content2 = client_1.read_from_file(file_name)
+    print(f"Content read from {file_name}: {content2}")
+
+    print(f"Removing file {file_name}")
+    client_1.delete_file(file_name)
+
+    print("Final listing")
+    client_1.list_server_files(verbose=True)
+    
+    #client_1.dump()
+
+# client paratemers for execution:
+# install - load all files from server
+# merge - compare files between client and server, and do the actions required to make them equals
+
+def run():
+    client = Client()
+    # Repita:
+    while True:
+        
+        # Se não existir lista local:
+        if client.client_prev_list == None:
+            
+            # Pegar lista do servidor
+            client.server_prev_list = client.list_server_files()
+            
+            # Para cada item da lista:
+            for file in client.server_prev_list:
+                # Solicitar arquivo para o servidor e criar no local
+                f = open(f"{client.file_dir}/{file['name']}", "w")
+                content = client.read_from_file(file['name'])
+                f.write(content)
+                f.close()
+            
+            # Salvar lista local 
+            client.client_prev_list = client.server_prev_list
+    
+        # Inicias sincronismo entre local e remoto:
+        else:
+            # Obter as listas atuais - local e servidor
+            client.client_cur_list = client.list_local_files()
+            client.server_cur_list = client.list_server_files()
+
+            # CASO 1- Novo arquivo no local
+            # percorre a lista local atual
+            for cur_file in client.client_cur_list:
+                # Supor que o arquivo não existe
+                exists = False
+                # procurar arquivo atual na lista anterior
+                for prev_file in client.client_prev_list:
+                    # se o elemento não existe na anteiror
+                    if prev_file['name'] == cur_file['name']:
+                        # encontrou o arquivo, sinalizar e interromper busca
+                        exists = True
+                        break
+
+                # Se o arquivo não existe
+                if not exists:
+                    # ler conteudo do arquivo
+                    #openedFile = open("./files/" + cur_file['name'], "r")
+                    #conteudo = openedFile.read()
+                    # adiciona na lista de execução (add_server, name_arquivo)
+                    client.operation_list.append(('add_server',cur_file['name']))
+
+    # -- -- # CASO 2- Novo arquivo no servidor
+    # -- -- percorre a lista do servidor atual
+    # -- -- -- se o elemento não existe na anterior
+    # -- -- -- -- adiciona na lista de execução (add_local, name_arquivo)
+    #
+
+    # -- -- # CASO 3 - arquivo removido no local
+    # -- -- percorre a lista do local anterior
+    # -- -- -- se o elemento não existe na atual
+    # -- -- -- -- adiciona na lista de execução (remove_server, name_arquivo)
+
+    # -- -- # CASO 4 - arquivo removido no servidor
+    # -- -- percorre a lista do servidor anterior
+    # -- -- -- se o elemento não existe na atual
+    # -- -- -- -- adiciona na lista de execução (remove_local, name_arquivo)
+
+    # -- -- Executar ações na lista
+            for operation in client.operation_list:
+                if(operation[0] == 'add_server'):
+                    # cria arquivo no servidor
+                    client.create_file(operation[1])
+                    client.write_to_file(operation[1], operation[2])
+                    client.upload(operation[1])
+                    print(f"Novo arquivo local enviado para o servidor - {operation[1]}")
+                
+                elif(operation[0] == 'add_local'):
+                    # cria arquivo local
+                    newFile = open("./files/" + operation[1], "x")
+                    newFile.write(operation[2])
+                    newFile.close()
+                
+                elif(operation[0] == 'remove_server'):
+                    # remove arquivo do servidor
+                    client.remove_file(operation[1])
+                
+                elif(operation[0] == 'remove_local'):
+                    # remove arquivo local
+                    os.remove("./files/" + operation[1])
+    # -- -- limpa lista de operações
+            client.operation_list = []
+            
+                    
+    # -- -- Listas atuais agora são as anteriores
+            client.client_prev_list = client.client_cur_list
+            client.server_prev_list = client.server_cur_list 
+
+            time.sleep(5) 
+
+if __name__ == "__main__":
+    #test()
+    run()
---
+++ SD/Sistema de arquivos/ServerDir/server.py
@@ -0,0 +1,153 @@
+# importar a biblioteca flask
+from flask import Flask, request, jsonify 
+# biblioteca CORS
+from flask_cors import CORS
+import os
+
+SERVER_DIR = "./files/"
+
+class File:
+    # construtor com valor padrão nos parâmetros
+    def __init__(self, name="", id="", modified=""):
+        self.name = name
+        self.id = id
+        self.modified = modified
+
+    # expressar a classe em formato texto
+    def __str__(self):
+        return f'{self.name}, '+\
+               f'{self.id}, {self.modified}'
+
+    # expressar a classe em formato json
+    def json(self):
+        return {
+            "name" : self.name,
+            "id" : self.id,
+            "modified" : self.modified 
+        }
+
+# acesso ao flask via variável app
+app = Flask(__name__)
+
+# inserindo a aplicação em um contexto
+# https://flask.palletsprojects.com/en/2.2.x/appcontext
+with app.app_context():
+
+    # aplicando tratamento CORS ao flask
+    # https://flask-cors.readthedocs.io/en/latest/
+    CORS(app)
+
+    # rota padrão
+    @app.route("/")
+    def ola():
+        return "<b>Olá, gente!</b>"
+
+    # rota de listar pessoas
+    @app.route("/listar")
+    def listar():
+        try:
+            # cria a lista de retorno que sera usadada para gerar o json
+            lista_retorno = []
+            # obtem lista dos arquivos do diretŕio atual
+            dirEntrys = os.scandir(SERVER_DIR)
+            lista = []
+            # percorre a lista de entradas
+            for entry in dirEntrys:
+                # obtem status referente ao arquivo e grava na lista
+                fileStatus = entry.stat()
+                file = File(entry.name, fileStatus.st_ino, fileStatus.st_mtime)
+                print(file)
+                lista.append(file)
+
+
+            # percorrer a lista de arquivos e tranforma em json
+            for file in lista:
+                lista_retorno.append(file.json())
+
+            # preparar uma parte da resposta: resultado ok
+            meujson = {"header":"OK"}
+
+            meujson.update({"files":lista_retorno})
+
+            # retornar a lista de pessoas json, com resultado ok
+            resposta = meujson
+
+            # trate corretamente esse erro
+        except Exception as e: 
+            resposta = jsonify({"header": "erro", "files": str(e)})
+
+        return resposta
+
+    @app.route("/criar/<file_name>")
+    def criar(file_name):
+        try:
+            newFile = open(SERVER_DIR + file_name, "x")
+            newFile.close()
+            entry = os.stat(SERVER_DIR + file_name)
+            file = File(file_name, entry.st_ino, entry.st_mtime)
+            resposta = jsonify({"header": "OK", "file": file.json()})
+        except Exception as e:
+            resposta = jsonify({"header": "erro", "detail": str(e)})
+
+        return resposta
+
+    @app.route("/deletar/<file_name>")
+    def deletar(file_name):
+        try:
+            os.remove(SERVER_DIR + file_name)
+            resposta = jsonify({"header": "OK", "deatil": file_name + ' deleted'})
+        except Exception as e:
+            resposta = jsonify({"header": "erro", "detail": str(e)})
+
+        return resposta
+
+    # Rota antiga - usando método GET
+    @app.route("/escrever_antigo/<file_name>/<conteudo>")
+    def escrever_antigo(file_name, conteudo):
+        try:
+            openedFile = open(SERVER_DIR + file_name, "w")
+            openedFile.write(conteudo)
+            openedFile.close()
+            print(conteudo)
+            resposta = jsonify({"header": "OK", "detail": "success!"})
+        except Exception as e:
+            resposta = jsonify({"header": "erro", "detail": str(e)})
+        return resposta
+    
+    # Para testar a rota: curl -i -X POST -F files=@nome_arquivo http://127.0.0.1:5000/escrever
+    @app.route("/escrever", methods=['POST'])
+    def escrever():
+        try:
+            f = request.files['files']
+            # print(f.filename)
+            f.save(SERVER_DIR + f.filename)
+            resposta = jsonify({"header": "OK", "detail": "success!"})
+        
+        except Exception as e:
+            resposta = jsonify({"header": "erro", "detail": str(e)})
+        
+        return resposta
+
+    @app.route("/ler/<file_name>")
+    def ler(file_name):
+        try:
+            openedFile = open(SERVER_DIR + file_name, "r")
+            conteudo = openedFile.read()
+            openedFile.close()
+            resposta = jsonify({"header": "OK", "detail": conteudo})
+        except Exception as e:
+            resposta = jsonify({"header": "erro", "detail": str(e)})
+        return resposta
+
+    app.run(debug=True, host='0.0.0.0')
+    # para depurar a aplicação web no VSCode, é preciso remover debug=True
+    # https://stackoverflow.com/questions/17309889/how-to-debug-a-flask-app
+
+
+'''
+resultado da invocação ao servidor:
+
+curl localhost:5000/listar
+
+
+'''
